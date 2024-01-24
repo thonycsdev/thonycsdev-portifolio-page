4f25659616318ef49a815f95003d49248617b62d
Anthony Coutinho <thonycout@gmail.com>
2024-01-24 12:42:22 +0000

    using cards component

--- src/components/CertificationCard.tsx
+++ src/components/CertificationCard.tsx
@@ -1,7 +1,30 @@
+import { Certifications } from "@/data/skills";
 import React from "react";
+import { PiCertificateDuotone } from "react-icons/pi";
 
-function CertificationCard() {
-  return <div>CertificationCard</div>;
+type Props = {
+  certification: Certifications;
+};
+
+function CertificationCard({ certification }: Props) {
+  return (
+    <li className="relative flex flex-row justify-between gap-3 items-center border border-emerald-600 rounded-xl p-2 transition-all duration-200 hover:scale-110 max-w-full overscroll-contain">
+      <div className="flex flex-col w-4/5">
+        <span className="text-emerald-400 text-xl font-semibold text-wrap">
+          {certification.name}
+        </span>
+        <span className="text-emerald-400 text-xl">
+          {certification.hours} Hrs
+        </span>
+        <span className="text-white text-lg">
+          {certification.dateStartedAndCompletion}
+        </span>
+      </div>
+      <div className="w-2/5 h-20 flex justify-center items-center">
+        <PiCertificateDuotone className="w-3/4 h-3/4" />
+      </div>
+    </li>
+  );
 }
 
 export default CertificationCard;
--- src/components/CertificationList.tsx
+++ src/components/CertificationList.tsx
@@ -1,6 +1,6 @@
 import { Certifications } from "@/data/skills";
 import React from "react";
-import { PiCertificateDuotone } from "react-icons/pi";
+import CertificationCard from "./CertificationCard";
 
 type Props = {
   certifications: Certifications[];
@@ -10,23 +10,7 @@ function CertificationList({ certifications }: Props) {
   return (
     <ul className="flex flex-col gap-5 mt-10">
       {certifications.map((cert, idx) => (
-        <li
-          key={idx}
-          className="relative flex flex-row justify-between gap-3 items-center border border-emerald-600 rounded-xl p-2 transition-all duration-200 hover:scale-110 max-w-full overscroll-contain"
-        >
-          <div className="flex flex-col w-4/5">
-            <span className="text-emerald-400 text-xl font-semibold text-wrap">
-              {cert.name}
-            </span>
-            <span className="text-emerald-400 text-xl">{cert.hours} Hrs</span>
-            <span className="text-white text-lg">
-              {cert.dateStartedAndCompletion}
-            </span>
-          </div>
-          <div className="w-2/5 h-20">
-            <PiCertificateDuotone className="w-full h-full" />
-          </div>
-        </li>
+        <CertificationCard certification={cert} key={idx} />
       ))}
     </ul>
   );
