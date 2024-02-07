206f33db396d702d2f4de837fcc627c0090f2b64
thonycsdev <thonycout@gmail.com>
2024-02-07 23:31:05 +0000

    adding more tests to `database.spec.ts`

--- __tests__/data/database.spec.ts
+++ __tests__/data/database.spec.ts
@@ -1,9 +1,17 @@
 import database from "@/data/database";
 
-
 describe("database.js tests", () => {
-    test("Given a empty query, should throw an error", async () => {
-        const result = await database.query('SHOW max_connections');
-        expect(result).toBeDefined();
-    });
-})
+  test("Should return the max connection of the database", async () => {
+    const result = await database.query("SHOW max_connections");
+    const max_connections = parseInt(result![0].max_connections);
+    expect(result).toBeDefined();
+    expect(max_connections).toBe(100);
+  });
+  test("Should return the current connection of the database", async () => {
+    const result = await database.query("SELECT * FROM pg_stat_activity");
+    expect(result).toBeDefined();
+  });
+  test("Should return a empty array when passing a empty value as the query", async () => {
+    expect(await database.query("")).toEqual([]);
+  });
+});
