bfba40f0ea669ed705214387c0a9c64089aa0eb6
thonycsdev <thonycout@gmail.com>
2024-02-07 23:22:55 +0000

    inserting local_database

--- __tests__/data/database.spec.ts
+++ __tests__/data/database.spec.ts
@@ -3,7 +3,7 @@ import database from "@/data/database";
 
 describe("database.js tests", () => {
     test("Given a empty query, should throw an error", async () => {
-        const result = await database.query('SELECT $1::text as message');
+        const result = await database.query('SHOW max_connections');
         expect(result).toBeDefined();
     });
 })
--- src/data/database.ts
+++ src/data/database.ts
@@ -1,22 +1,27 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
-async function query(queryObject:string){
-    const client = new Client();
-    try {
-        await client.connect();
-        const response = await client.query(queryObject);
-        return response.rows;
-    } catch (error) {
-        console.error('Error executing query', error);
-    }
-    finally {
-        await client.end();
-    }
-    return;
+async function query(queryObject: string) {
+  const client = new Client({
+    database: "postgres",
+    user: "postgres",
+    password: "postgres",
+    host: "localhost",
+    port: 5432,
+  });
+  try {
+    await client.connect();
+    const response = await client.query(queryObject);
+    return response.rows;
+  } catch (error) {
+    console.error("Error executing query", error);
+  } finally {
+    await client.end();
+  }
+  return;
 }
 
+const database = {
+  query,
+};
 
-
-export default {
-    query: query
-}
+export default database;
