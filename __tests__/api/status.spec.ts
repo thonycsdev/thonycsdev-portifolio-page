ce652d3bed585e7abe379e0213fcba072287a6de
thonycsdev <thonycout@gmail.com>
2024-02-08 15:54:44 +0000

    status endpoint

---
+++ __tests__/api/status.spec.ts
@@ -0,0 +1,11 @@
+describe("status.js tests", () => {
+  test("Should return the status bject when called", async () => {
+    var response = await fetch("http://localhost:3000/api/v1/status");
+    var status = await response.json();
+    expect(status).toBeDefined();
+    expect(status.updated_at).toBeDefined();
+    expect(status.dependencies.database.version).toBe("16.1");
+    expect(status.dependencies.database.max_connections).toBe("100");
+    expect(status.dependencies.database.opened_connections).toBe(1);
+  });
+});
--- src/data/database.ts
+++ src/data/database.ts
@@ -1,6 +1,6 @@
 import { Client } from "pg";
 
-async function query(queryObject: string) {
+async function query(queryObject: { text: string; values?: any[] } | string) {
   const client = new Client({
     database: "postgres",
     user: "postgres",
@@ -20,8 +20,40 @@ async function query(queryObject: string) {
   return;
 }
 
+async function getDatabaseVersion() {
+  const databaseVersionResult = (await database.query(
+    "SHOW server_version;"
+  )) as any[];
+  const database_version = formatDatabaseVersion(
+    databaseVersionResult[0].server_version
+  );
+  return database_version;
+}
+
+function formatDatabaseVersion(version: string) {
+  return version.split(" ")[0];
+}
+async function getDatabaseMaxConnections() {
+  var databaseConnectionsResult = await database.query("SHOW max_connections");
+  var max_connections = databaseConnectionsResult![0].max_connections;
+  return max_connections;
+}
+
+async function getOpenedConnections() {
+  const databaseName = "postgres";
+  const activeConnections = (await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  })) as any[];
+  const activeConnectionsValue = activeConnections[0].count;
+  return activeConnectionsValue;
+}
+
 const database = {
   query,
+  getDatabaseMaxConnections,
+  getDatabaseVersion,
+  getOpenedConnections,
 };
 
 export default database;
