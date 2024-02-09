3482066982ebcaef0b604a41ac0bc41b25b95698
thonycsdev <thonycout@gmail.com>
2024-02-09 15:08:40 +0000

    migrations done, with seeds

---
+++ infra/scripts/seed-experiences-database.sql
@@ -0,0 +1,15 @@
+/* INSERT QUERY NO: 1 */
+INSERT INTO experiences(name, level, description, started_year, ended_year)
+VALUES ('Impar', 'Junior', 'Full Stack Developer', '11/2022', '11/2023');
+
+/* INSERT QUERY NO: 2 */
+INSERT INTO experiences(name, level, description, started_year, ended_year)
+VALUES ('Edubrahub', 'Junior', 'Full Stack Developer', '06/2022', '02/2023');
+
+/* INSERT QUERY NO: 3 */
+INSERT INTO experiences(name, level, description, started_year, ended_year)
+VALUES ('Freelancer', 'Junior', 'Full Stack Developer', '06/2020', '06/2021');
+
+/* INSERT QUERY NO: 4 */
+INSERT INTO experiences(name, level, description, started_year, ended_year)
+VALUES ('VD Tecnologia', 'Estagiário', 'Backend Developer', '03/2020', '06/2020');
