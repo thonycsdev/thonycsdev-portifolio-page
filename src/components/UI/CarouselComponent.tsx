d2eec22eed033d9a8a5ccc45bf8d977b253f06f8
Anthony Silva <anthony.eldyn@gmail.com>
2024-10-01 15:59:26 -0300

    Ajustando constantes

--- src/components/UI/CarouselComponent.tsx
+++ src/components/UI/CarouselComponent.tsx
@@ -1,16 +1,11 @@
 
-interface CarouselItemProps {
-  src: string;
-  alt: string;
-}
-
 function CarouselComponent() {
   return (
-    <div className="grid grid-rows-4 grid-cols-1 gap-3">
-      <div className="flex justify-center items-center bg-paper-darker h-16 rounded-xl border-black border"><img className="h-full " src="./csharp_logo.png" /> CSharp</div>
-      <div className="flex justify-center items-center bg-paper-darker h-16 rounded-xl border-black border"><img className="h-full p-3" src="./net_core_logo.png" /> CSharp</div>
-      <div className="flex justify-center items-center bg-paper-darker h-16 rounded-xl border-black border"><img className="h-full p-3" src="./js_logo.png" /> CSharp</div>
-      <div className="flex justify-center items-center bg-paper-darker h-16 rounded-xl border-black border"><img className="h-full p-3" src="./react_icon.png" /> CSharp</div>
+    <div className="grid grid-rows-4 grid-cols-1 gap-3 justify-items-center">
+      <div className="grid grid-cols-2 bg-paper-darker h-16 rounded-xl border-black border items-center justify-items-center"><img className="h-full" src="./csharp_logo.png" /> CSharp</div>
+      <div className="grid grid-cols-2 bg-paper-darker h-16 rounded-xl border-black border items-center justify-items-center"><img className="h-full p-3" src="./net_core_logo.png" />.NET Core</div>
+      <div className="grid grid-cols-2 bg-paper-darker h-16 rounded-xl border-black border items-center justify-items-center"><img className="h-full p-3" src="./js_logo.png" />JavaScript</div>
+      <div className="grid grid-cols-2 bg-paper-darker h-16 rounded-xl border-black border items-center justify-items-center"><img className="h-full p-3" src="./react_icon.png" />React</div>
     </div>
   );
 }
