630451003e1a15f9e35d92cba5b706b072818b88
Anthony Silva <anthony.eldyn@gmail.com>
2024-10-01 10:43:16 -0300

    Adicionando nova fonte ao projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import AboutSection from "@/components/AboutSection";
+import HeroSection from "@/components/HeroSection";
 import HomeCardComponent from "@/components/UI/HomeCardComponent";
 
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
     <main className="h-full md:h-screen bg-paper-background flex flex-col justify-center items-start p-10 ">
       <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-5 gap-3">
         <div className="row-span-2 md:row-span-3 ">
-          <HomeCardComponent content={<AboutSection />} />
+          <HomeCardComponent content={<HeroSection />} />
         </div>
         <div>
           <HomeCardComponent content={<AboutSection />} />
