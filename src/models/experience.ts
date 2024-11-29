46ab243b357206e22a904a908f81457c930b80a4
Anthony <anthony.eldyn@gmail.com>
2024-11-29 20:54:01 -0300

    fix: fix error in Experience model

--- src/models/experience.ts
+++ src/models/experience.ts
@@ -1,16 +1,5 @@
 import { BaseModel } from './baseMode';
 
-export class Experience2 extends BaseModel {
-	started_year: string;
-	ended_year: string | null;
-
-	constructor(data: Experience) {
-		super(data);
-		this.started_year = data.started_year;
-		this.ended_year = data.ended_year;
-	}
-}
-
 export interface Experience extends BaseModel {
 	started_year: string;
 	ended_year: string | null;
